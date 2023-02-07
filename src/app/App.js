44ed806e8058852f73aac22188631dd63fffe075
celiyo <riana.tysson@gmail.com>
2023-02-07 19:06:56 +0000

    Update the App.js file

--- src/app/App.js
+++ src/app/App.js
@@ -1,3 +1,23 @@
-function App() {}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/projects/:id' element={<Project />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
